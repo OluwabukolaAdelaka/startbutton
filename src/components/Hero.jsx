e8b60b701050bc706fc5b7bd403387c6ea13273e
Grace Adelaka <adelakagrace@gmail.com>
2022-05-23 14:52:45 +0100

    updated navbar and hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,29 +3,34 @@ import Group from "../assets/Group.png";
 
 const Hero = () => {
   return (
-    <div className="max-w-[1240px] w-full h-screen mx-auto py-24 pl- flex">
-      <div className="relative left-[400px] top-[146px] right-0">
-        <img src={Group} alt="group" />
+    <div className="max-w-[1240px] w-full h-screen grid md:grid-cols-2 mx-auto px-4 pt-16">
+      {/* image part */}
+      <div className="relative left-[0px] top-[400px] md:left-[278px] md:top-[106px] lg:left-[508px] right-0">
+        <img src={Group} alt="group" className="object-fit" />
       </div>
+      {/* text-part */}
       <div className="text-left absolute">
-        <h1 className="text-[#03071e] w-[60%] font-custom2 text-8xl font-extrabold">
+        <h1 className="text-[#03071e] w-[80%] lg:w-[60%] font-custom2 text-3xl md:text-6xl font-extrabold">
           Your Gateway to Emerging Markets
         </h1>
-        <p className="text-[#727987] w-[40%] font-custom1 text-[26px] leading-[35px]  mt-2 py-8 ">
+        <p className="text-[#727987] w-[80%] lg:w-[55%] font-custom1 text-[26px] leading-[35px] mt-1 py-5 ">
           Startbutton is a digital platform that enables foreign businesses to
           sell to Africa while enabling African merchants to sell to the rest of
           the world.
         </p>
-        <div className="bg-white border rounded-md mt-4 p-2 w-[30%]">
+        <div className="block md:flex bg-white border border-slate-900 rounded-md mt-4 p-2 w-[70%] lg:w-[50%]">
           <input
             type="text"
-            className="w-[60%] pl-2 text-lg"
+            className="w-[70%] pl-2 text-lg"
             placeholder="Enter your email"
           />
-          <button className="text-white bg-[#03071e] font-custom1 w-[40%] font-bold text-sm text-center border rounded-md p-3">
+          <button className="hidden md:block text-white bg-[#03071e] font-custom1 w-[30%] font-bold text-sm text-center border rounded-md p-3">
             Join the waitlist
           </button>
         </div>
+        <button className="md:hidden text-white bg-[#03071e] font-custom1 w-[40%] font-bold text-sm text-center border rounded-md p-3 mt-2">
+          Join the waitlist
+        </button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/Logo.png";
+import LogoMob from "../assets/LogoMob.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className="max-w-[1240px] flex justify-between mx-auto h-24 py-8 items-center">
-      <img src={Logo} alt="logo" />
-      <ul className="flex ">
-        <li className="font-bold text-lg p-6">Services</li>
-        <li className="font-bold text-lg p-6">Resources</li>
-        <li className="font-bold text-lg p-6">Blog</li>
+    <div className="max-w-[1240px] flex justify-between mx-auto h-24 pt-4 px-4 items-center">
+      {/* main menu */}
+      <img src={Logo} alt="logo" className="hidden md:flex" />
+      <ul className="hidden md:flex ">
+        <li className="font-bold font-custom2 text-sm lg:text-lg p-6">
+          Services
+        </li>
+        <li className="font-bold font-custom2 text-sm lg:text-lg p-6">
+          Resources
+        </li>
+        <li className="font-bold font-custom2 text-sm lg:text-lg p-6">Blog</li>
       </ul>
-      <button className="text-white text-md w-[150px] py-2 border rounded-md bg-gradient-to-r from-[#de3300] to-[#e85d04]">
+      <button className="hidden md:block text-white font-custom2 text-md w-[150px] md:w-[150px] py-2 border rounded-md bg-gradient-to-r from-[#de3300] to-[#e85d04]">
+        Push to Start!
+      </button>
+      {/* mobile menu */}
+      <div className="flex md:hidden">
+        <div onClick={handleNav}>
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+        <img src={LogoMob} alt="logo" className="pl-5" />
+      </div>
+      <ul
+        className={
+          nav
+            ? "md:hidden fixed top-[12%] border left-0 bg-white w-[200px] h-[200px] ease-in-out duration-1000 z-10"
+            : "ease-in-out duration-1000 fixed top-[10%] left-[-100%]"
+        }
+      >
+        <li className="font-bold font-custom2 text-lg p-3">Services</li>
+        <li className="font-bold font-custom2 text-lg p-3">Resources</li>
+        <li className="font-bold font-custom2 text-lg p-3">Blog</li>
+      </ul>
+      <button className="md:hidden text-white text-sm w-[100px] py-2 border rounded-md bg-gradient-to-r from-[#de3300] to-[#e85d04]">
         Push to Start!
       </button>
     </div>
